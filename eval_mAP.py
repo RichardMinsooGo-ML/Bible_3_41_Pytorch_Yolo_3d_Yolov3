6ee2763a1ae5313e577c137ff8542660b9d1e39c
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2021-06-27 17:39:59 +0900

    Add files via upload

---
+++ eval_mAP.py
@@ -0,0 +1,123 @@
+# python eval_mAP.py --model_def config/cfg/yolo3d_yolov3.cfg --pretrained_path checkpoints/Model_yolo3d_yolov3.pth
+# python eval_mAP.py --model_def config/cfg/yolo3d_yolov3_tiny.cfg --pretrained_path checkpoints/Model_yolo3d_yolov3_tiny.pth
+
+import os, sys, time, datetime, argparse
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import numpy as np
+import torch
+import torch.utils.data.distributed
+import tqdm
+sys.path.append("./")
+
+from data_process.kitti_dataloader import create_val_dataloader
+
+# from models.model_utils import create_model, make_data_parallel
+from models.models import *
+
+from utils.evaluation_utils import get_batch_statistics_rotated_bbox, ap_per_class, load_classes, post_processing_v2
+
+def evaluate_mAP(val_loader, model, configs):
+    # switch to evaluate mode
+    model.eval()
+
+    labels = []
+    sample_metrics = []  # List of tuples (TP, confs, pred)
+    with torch.no_grad():
+        # start_time = time.time()
+        for batch_idx, batch_data in enumerate(tqdm.tqdm(val_loader)):
+            # data_time.update(time.time() - start_time)
+            _, imgs, targets = batch_data
+            # Extract labels
+            labels += targets[:, 1].tolist()
+            # Rescale x, y, w, h of targets ((box_idx, class, x, y, z, h, w, l, im, re))
+            targets[:, 2:4] *= configs.img_size
+            targets[:, 5:8] *= configs.img_size
+            imgs = imgs.to(configs.device, non_blocking=True)
+
+            outputs = model(imgs)
+            outputs = post_processing_v2(outputs, conf_thresh=configs.conf_thres, nms_thresh=configs.nms_thres)
+
+            sample_metrics += get_batch_statistics_rotated_bbox(outputs, targets, iou_threshold=configs.iou_thres)
+
+        # Concatenate sample statistics
+        true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
+        precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
+
+    return precision, recall, AP, f1, ap_class
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--model_def", type=str, default="config/cfg/yolo3d_yolov3.cfg", metavar="PATH", help="The path for cfgfile (only for darknet)")
+    parser.add_argument("--pretrained_path", type=str, default="checkpoints/Model_yolo3d_yolov3.pth", metavar="PATH", help="the path of the pretrained checkpoint")
+    
+    # parser.add_argument("--model_def", type=str, default="config/cfg/yolo3d_yolov3_tiny.cfg", metavar="PATH", help="The path for cfgfile (only for darknet)")
+    # parser.add_argument("--pretrained_path", type=str, default="checkpoints/Model_yolo3d_yolov3_tiny.pth", metavar="PATH", help="the path of the pretrained checkpoint")
+    
+    parser.add_argument("-a", "--arch", type=str, default="darknet", metavar="ARCH", help="The name of the model architecture")
+    parser.add_argument("--batch_size"  , type=int  , default=4, help="size of each image batch")
+    
+    parser.add_argument("--use_giou_loss", action="store_true", help="If true, use GIoU loss during training. If false, use MSE loss for training")
+
+    parser.add_argument("--no_cuda", action="store_true", help="If true, cuda is not used.")
+    parser.add_argument("--gpu_idx", default=None, type=int, help="GPU index to use.")
+
+    parser.add_argument("--num_samples", type=int, default=None, help="Take a subset of the dataset to run and debug")
+    parser.add_argument("--num_workers", type=int, default=4, help="Number of threads for loading data")
+    parser.add_argument("--class_path", type=str, default="dataset/kitti/classes_names.txt", metavar="PATH", help="The class names of objects in the task")
+    parser.add_argument("--iou_thres"   , type=float, default=0.5, help="iou threshold required to qualify as detected")
+    parser.add_argument("--conf_thres"  , type=float, default=0.5, help="object confidence threshold")
+    parser.add_argument("--nms_thres"   , type=float, default=0.5, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--img_size",   type=int,   default=608, help="the size of input image")
+
+    configs = parser.parse_args()
+    configs.pin_memory = True
+
+    ####################################################################
+    ##############Dataset, Checkpoints, and results dir configs#########
+    ####################################################################
+    configs.working_dir = "./"
+    configs.dataset_dir = os.path.join(configs.working_dir, "dataset", "kitti")
+
+    configs.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # configs.device = torch.device("cpu" if configs.no_cuda else "cuda:{}".format(configs.gpu_idx))
+    model = Darknet(configs.model_def, img_size=configs.img_size).to(configs.device)
+    configs.distributed = False  # For evaluation
+    
+    # Get data configuration
+    class_names = load_classes(configs.class_path)
+
+    # Data Parallel
+    # model = make_data_parallel(model, configs)
+    
+    # model.print_network()
+    print("\n" + "___m__@@__m___" * 10 + "\n")
+    
+    print(configs.pretrained_path)
+    
+    assert os.path.isfile(configs.pretrained_path), "No file at {}".format(configs.pretrained_path)
+    
+    # If specified we start from checkpoint
+    if configs.pretrained_path:
+        if configs.pretrained_path.endswith(".pth"):
+            model.load_state_dict(torch.load(configs.pretrained_path))
+            print("Trained pytorch weight loaded!")
+        else:
+            model.load_darknet_weights(configs.pretrained_path)
+            print("Darknet weight loaded!")
+        
+    model.eval()
+        
+    print("Create the validation dataloader")
+    val_dataloader = create_val_dataloader(configs)
+
+    print("\nStart computing mAP...\n")
+    precision, recall, AP, f1, ap_class = evaluate_mAP(val_dataloader, model, configs)
+
+    print("\nDone computing mAP...\n")
+    for idx, cls in enumerate(ap_class):
+        print("\t>>>\t Class {} ({}): precision = {:.4f}, recall = {:.4f}, AP = {:.4f}, f1: {:.4f}".format(cls, \
+                class_names[cls][:3], precision[idx], recall[idx], AP[idx], f1[idx]))
+
+    print("\nmAP: {:.4}\n".format(AP.mean()))
---
+++ train.py
@@ -0,0 +1,203 @@
+# python train.py --model_def config/cfg/yolo3d_yolov3.cfg --pretrained_path checkpoints/Model_yolo3d_yolov3.pth --save_path checkpoints/Model_yolo3d_yolov3.pth
+# python train.py --model_def config/cfg/yolo3d_yolov3.cfg --pretrained_path checkpoints/yolov3.weights --save_path checkpoints/Model_yolo3d_yolov3.pth
+
+# python train.py  --model_def config/cfg/yolo3d_yolov3_tiny.cfg --pretrained_path checkpoints/Model_yolo3d_yolov3_tiny.pth --save_path checkpoints/Model_yolo3d_yolov3_tiny.pth
+# python train.py  --model_def config/cfg/yolo3d_yolov3_tiny.cfg --pretrained_path checkpoints/yolov3-tiny.weights --save_path checkpoints/Model_yolo3d_yolov3_tiny.pth
+
+from terminaltables import AsciiTable
+
+import os, sys, time, datetime, argparse
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import torch
+
+import tqdm
+from data_process.kitti_dataloader import create_train_dataloader, create_val_dataloader
+
+# from models.model_utils import create_model, make_data_parallel
+from models.models import *
+from utils.evaluation_utils import load_classes
+from config.train_config import parse_train_configs
+import torch.optim as optim
+from eval_mAP import evaluate_mAP
+
+import pickle
+
+def main():
+    
+    configs = parse_train_configs()    
+
+    ############## Hardware configurations #############################    
+    configs.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Initiate model
+    model = Darknet(configs.model_def, img_size=configs.img_size).to(configs.device)
+    model.apply(weights_init_normal)
+    
+    # Get data configuration
+    class_names = load_classes("dataset/classes.names")
+
+    """
+    idx_cnt = 0
+    for name, param in model.named_parameters():
+        # module_list.21.batch_norm_21.bias
+        layer_id = int(name.split('.')[1])
+        print(idx_cnt,name, layer_id)
+        idx_cnt += 1
+    idx_cnt = 0
+    for param in model.parameters():
+        print(idx_cnt, param.requires_grad)
+        idx_cnt += 1
+    """
+    
+    # If specified we start from checkpoint
+    if configs.pretrained_path:
+        if configs.pretrained_path.endswith(".pth"):
+            model.load_state_dict(torch.load(configs.pretrained_path))
+            print("Trained pytorch weight loaded!")
+        else:
+            model.load_darknet_weights(configs.pretrained_path)
+            print("Darknet weight loaded!")
+
+    optimizer = torch.optim.Adam(model.parameters())
+
+    # learning rate scheduler config
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.8)
+        
+    f = open('checkpoints/global_epoch.pckl', 'rb')
+    global_epoch = pickle.load(f)
+    f.close()
+    
+    # Create dataloader
+    # train_dataloader, train_sampler = create_train_dataloader(configs)
+    train_dataloader = create_val_dataloader(configs)
+    
+    max_mAP_max = 0.5
+        
+    for epoch in range(0, configs.num_epochs, 1):
+            
+        num_iters_per_epoch = len(train_dataloader)        
+
+        # print(num_iters_per_epoch)
+
+        # switch to train mode
+        model.train()
+        # start_time = time.time()
+        
+        epoch_loss = 0
+        # Training        
+        for batch_idx, batch_data in enumerate(tqdm.tqdm(train_dataloader)):
+            """
+            # print(batch_data)
+            
+            print(batch_data[0])
+            print(batch_data[1])
+            print(batch_data[1].shape)
+            print(batch_data[2])
+            
+            imgs = batch_data[1]
+            
+            from PIL import Image
+            import numpy as np
+
+            w, h = imgs[0].shape[1], imgs[0].shape[2]
+            src = imgs[0]
+            # data = np.zeros((h, w, 3), dtype=np.uint8)
+            # data[256, 256] = [255, 0, 0]
+            
+            data = np.zeros((h, w, 3), dtype=np.uint8)
+            data[:,:,0] = src[0,:,:]*255
+            data[:,:,1] = src[1,:,:]*255
+            data[:,:,2] = src[2,:,:]*255
+            # img = Image.fromarray(data, 'RGB')
+            img = Image.fromarray(data)
+            img.save('my_img.png')
+            img.show()
+
+            import sys
+            sys.exit()
+            """
+            
+            # data_time.update(time.time() - start_time)
+            _, imgs, targets = batch_data
+            global_step = num_iters_per_epoch * epoch + batch_idx + 1
+            
+            # batch_size = imgs.size(0)
+
+            targets = targets.to(configs.device, non_blocking=True)
+            imgs = imgs.to(configs.device, non_blocking=True)
+            total_loss, outputs = model(imgs, targets)
+            
+            epoch_loss += float(total_loss.item())
+            # compute gradient and perform backpropagation
+            total_loss.backward()
+
+            if global_step % configs.gradient_accumulations:
+                # Accumulates gradient before each step
+                optimizer.step()
+                # Adjust learning rate
+                lr_scheduler.step()
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+            else:
+                reduced_loss = total_loss.data
+                
+            # ----------------
+            #   Log progress
+            # ----------------
+            
+        crnt_epoch_loss = epoch_loss/num_iters_per_epoch
+        # Evaulation        
+        
+        torch.save(model.state_dict(), configs.save_path)
+        global_epoch += 1
+        
+        print("Global_epoch :",global_epoch, "/ loss : {:1.5f}".format(crnt_epoch_loss),'Saved at {}'.format(configs.save_path))
+        
+        f = open('checkpoints/global_epoch.pckl', 'wb')
+        pickle.dump(global_epoch, f)
+        f.close()
+        
+                    
+    #-------------------------------------------------------------------------------------
+        """            
+
+            # Save checkpoint
+            if max_mAP_new > max_mAP_max:
+                torch.save(model.state_dict(), configs.save_path)
+                print('save a checkpoint at {}'.format(configs.save_path))
+                max_mAP_max = max_mAP_new
+            else:
+                model.load_state_dict(torch.load(configs.pretrained_path))
+                print("Max mAP weight will be used again!")
+        """            
+        
+    # Evaulation        
+    #-------------------------------------------------------------------------------------        
+    # if (epoch+1) % 8 == 0:
+
+    print("\n---- Evaluating Model ----")
+    val_dataloader = create_val_dataloader(configs)
+    precision, recall, AP, f1, ap_class = evaluate_mAP(val_dataloader, model, configs)
+
+    val_metrics_dict = {
+        'precision': precision.mean(),
+        'recall': recall.mean(),
+        'AP': AP.mean(),
+        'f1': f1.mean(),
+        'ap_class': ap_class.mean()
+    }
+
+    # Print class APs and mAP
+    ap_table = [["Index", "Class name", "AP"]]
+    for i, c in enumerate(ap_class):
+        ap_table += [[c, class_names[c], "%.5f" % AP[i]]]
+    print(AsciiTable(ap_table).table)
+    print(f"---- mAP {AP.mean()}")
+
+    max_mAP_new = AP.mean()
+    
+if __name__ == '__main__':
+    main()
+    
